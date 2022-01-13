b6b88b535816e9ea433d15bae60294457d9f7a9c
Daniel Hindrikes <daniel@hindrikes.se>
2022-01-13 21:02:17 +0100

    set binding context

---
+++ CodeSnippets/XamlAndMvvm/MainView.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using CodeSnippets.XamlAndMvvm;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+
+namespace MauiApp1
+{
+	//<SetBindingContext>
+	public partial class MainView : ContentPage
+	{
+		public MainView()
+		{
+			InitializeComponent();
+
+			BindingContext = new MainViewModel();
+		}
+	}
+	//</SetBindingContext>
+}
