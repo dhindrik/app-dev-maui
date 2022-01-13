34e4cdd8f2035e70c71131de044f9e9b65871bee
Daniel Hindrikes <daniel@hindrikes.se>
2022-01-13 20:51:47 +0100

    intro viewmodel

---
+++ CodeSnippets/XamlAndMvvm/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace CodeSnippets.XamlAndMvvm
+{
+    //<IntroViewModel>
+	public class MainViewModel : INotifyPropertyChanged
+	{
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string text;
+        public string Text
+        {
+            get => text;
+            set
+            {
+                if(text != value)
+                {
+                    text = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text)));
+                }
+            }
+        }
+    }
+    //</IntroViewModel>
+}
+
